2a4e686b0b2362cbaaea2310832a46b375db8c8e
KUBO, Koichi <k@obuk.org>
2012-07-25 22:43:37 +0900

    reviewing t/

---
+++ support/cover.sh
@@ -0,0 +1,3 @@
+HARNESS_PERL_SWITCHES=-MDevel::Cover PERL_DL_NONLAZY=1 /usr/bin/perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')" $*
+cover
+find cover_db -type d |xargs chmod +rx
