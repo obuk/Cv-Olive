969ba92862037dc85d935f8263c0a67887921fda
KUBO, Koichi <k@obuk.org>
2012-10-09 22:13:43 +0900

    fixed Cv::Flipbook has memory leak

--- lib/Cv/Flipbook.pm
+++ lib/Cv/Flipbook.pm
@@ -86,10 +86,6 @@ sub list {
 	wantarray ? @files : \@files;
 }
 
-sub DESTROY {
-	undef;
-}
-
 { *Grab = \&GrabFrame }
 sub GrabFrame {
 	my $self = shift;
@@ -156,41 +152,19 @@ Cv::Flipbook - Perl extension for blah blah blah
 
 =head1 SYNOPSIS
 
-  use Cv::Flipbook;
-  blah blah blah
-
 =head1 DESCRIPTION
-
-Stub documentation for Cv::Flipbook, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
 
 =head2 EXPORT
 
-None by default.
-
-
-
 =head1 SEE ALSO
-
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
 
 =head1 AUTHOR
 
-KUBO Koichi, E<lt>k@obuk.orgE<gt>
+MASUDA Yuta E<lt>yuta.cpan@gmail.comE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2012 by KUBO Koichi
+Copyright (C) 2012 by MASUDA Yuta
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.14.2 or,
