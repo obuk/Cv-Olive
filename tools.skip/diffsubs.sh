7a003d159d0dc2dd67ffe90e706aaba85cd7ec27
KUBO, Koichi <k@obuk.org>
2012-12-25 21:10:18 +0900

    Revert "fixed MANIFEST"
    This reverts commit f6932927a1ef7726002183d096a6cd61ff409a9f.

---
+++ tools.skip/diffsubs.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cwd=`pwd`
+# left=Cv-0.12
+left=Cv-0.13
+right=Cv-Olive
+for cv in $left $right; do
+    (cd ../$cv; $cwd/tools/listsubs.pl) |sort >$cv.tmp
+done
+diff -W 80 -y $left.tmp $right.tmp
